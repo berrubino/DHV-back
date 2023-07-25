8f7db5bdd36edd8f5c8afaa09539cf945845634e
berrubino <berni.rubino@gmail.com>
2023-07-25 16:30:59 -0300

    feat:categories models created

--- models/index.js
+++ models/index.js
@@ -2,5 +2,6 @@ const User = require("./User");
 const Visit = require("./Visit");
 const Properties = require("./Properties");
 const Favorites = require("./Favorites");
+const Categories = require("./Categories");
 
-module.exports = { User, Visit, Properties, Favorites };
+module.exports = { User, Visit, Properties, Favorites, Categories };
